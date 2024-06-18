119597c6f0a28242da233a47ea4a5b5fcabd58d5
tristanGIANDO <giando.tristan@gmail.com>
2024-06-18 15:13:02 +0200

    ratings

--- booking_scraper/booking_scraper/spiders/booking.py
+++ booking_scraper/booking_scraper/spiders/booking.py
@@ -1,5 +1,6 @@
 import scrapy
 
+
 class BookingSpider(scrapy.Spider):
     name = "booking"
     allowed_domains = ["booking.com"]
@@ -13,20 +14,19 @@ class BookingSpider(scrapy.Spider):
     }
 
     def parse(self, response):
-        self.log(f'Parsing page: {response.url}')
-        
-        hotels = response.css('div[data-testid="property-card"] div[data-testid="title"]')
-        self.log(f'Found {len(hotels)} hotels')
-        
-        for hotel in hotels[:20]:
-            name = hotel.css('::text').get().strip()
-            self.log(f'Hotel name: {name}')
+
+        hotels = response.css('div[data-testid="property-card"]')
+        print(f'Found {len(hotels)} hotels')
+
+        for hotel in hotels:
+            name = hotel.css('div[data-testid="title"]::text').get().strip()
+            rating = hotel.css('div.f13857cc8c.e008572b71::text').get()
+
             yield {
-                'name': name
+                'name': name,
+                'rating': rating
             }
 
-        # Pagination (si nécessaire)
         next_page = response.css('a.bui-pagination__link.pagenext::attr(href)').get()
         if next_page:
             yield response.follow(next_page, self.parse)
-
