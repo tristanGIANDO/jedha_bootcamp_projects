5b8982782584cd8767c37d15c547f44d04c0f0a4
tristanGIANDO <giando.tristan@gmail.com>
2024-06-19 17:06:58 +0200

    scrap all cities !

--- bloc_01_Data_Infrastructure/plan_your_trip_with_kayak/work/booking_scraper/booking_scraper/spiders/booking.py
+++ bloc_01_Data_Infrastructure/plan_your_trip_with_kayak/work/booking_scraper/booking_scraper/spiders/booking.py
@@ -1,13 +1,49 @@
 import scrapy
 
+CITIES = [
+    "Mont Saint Michel",
+    "St Malo",
+    "Bayeux",
+    "Le Havre",
+    "Rouen",
+    "Paris",
+    "Amiens",
+    "Lille",
+    "Strasbourg",
+    "Chateau du Haut Koenigsbourg",
+    "Colmar",
+    "Eguisheim",
+    "Besancon",
+    "Dijon",
+    "Annecy",
+    "Grenoble",
+    "Lyon",
+    "Gorges du Verdon",
+    "Bormes les Mimosas",
+    "Cassis",
+    "Marseille",
+    "Aix en Provence",
+    "Avignon",
+    "Uzes",
+    "Nimes",
+    "Aigues Mortes",
+    "Saintes Maries de la mer",
+    "Collioure",
+    "Carcassonne",
+    "Ariege",
+    "Toulouse",
+    "Montauban",
+    "Biarritz",
+    "Bayonne",
+    "La Rochelle"
+]
+
 
 class BookingSpider(scrapy.Spider):
     name = "booking"
     allowed_domains = ["booking.com"]
-    start_urls = [
-        "https://www.booking.com/searchresults.html?ss=Montpellier",
-        "https://www.booking.com/searchresults.html?ss=Grenoble"
-    ]
+    cities = ["montpellier"]
+    start_urls = [f"https://www.booking.com/searchresults.html?ss={city}" for city in CITIES]
 
     custom_settings = {
         "USER_AGENT": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
@@ -18,22 +54,27 @@ class BookingSpider(scrapy.Spider):
         hotels = response.css('div[data-testid="property-card"]')
 
         for hotel in hotels:
+            city = response.url.split("=")[-1]
             name = hotel.css('div[data-testid="title"]::text').get().strip()
             rating = hotel.css("div.f13857cc8c.e008572b71::text").get()
+            if not rating:
+                rating = "0.0"
             url = hotel.css('a[data-testid="title-link"]::attr(href)').get()
 
             yield response.follow(url, callback=self.parse_hotel_detail,
                                   meta={"name": name,
                                         "rating": rating,
-                                        "url": url})
+                                        "city": city
+                                        })
 
         next_page = response.css("a.bui-pagination__link.pagenext::attr(href)").get()
         if next_page:
             yield response.follow(next_page, self.parse)
 
     def parse_hotel_detail(self, response):
+        city = response.meta["city"]
         name = response.meta["name"]
-        url = response.meta["url"]
+        url = response.url
         rating = response.meta["rating"]
         address = response.css("span.hp_address_subtitle.js-hp_address_subtitle.jq_tooltip::text").get().strip()
         description = response.css('p[data-testid="property-description"]::text').get().strip()
@@ -48,6 +89,7 @@ class BookingSpider(scrapy.Spider):
             longitude = None
 
         yield {
+            "city": city,
             "name": name,
             "url": url,
             "rating": rating,
